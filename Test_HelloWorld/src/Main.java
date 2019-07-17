8af6836c3386619da9c2e3e111c22c8fc853fd1e
maxmihailovski <maxmihailovski@mail.ru>
2019-07-17 16:09:52 +0600

    second commit

---
+++ Test_HelloWorld/src/Main.java
@@ -0,0 +1,15 @@
+//import javax.swing.*;
+
+//public class Main {
+
+ //   public static void main(String[] args) {
+//        System.out.println("Hello World!");
+//    }
+//}
+
+import javax.swing.*;
+public class Main{
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null, "Все Ок?!");
+    }
+}
