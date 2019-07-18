fa29512a5cd1ad81ec9bf87a284a2ea624ab3944
maxmihailovski <maxmihailovski@mail.ru>
2019-07-18 17:52:30 +0600

    '0.2v'

--- Test_HelloWorld/src/Main.java
+++ Test_HelloWorld/src/Main.java
@@ -7,9 +7,25 @@
 //    }
 //}
 
+//import javax.swing.*;
+//public class Main{
+//    public static void main(String[] args){
+//        JOptionPane.showMessageDialog(null, "Все Ок?!"); //вызов диалогового окна
+//
+//    }
+//}
+
 import javax.swing.*;
-public class Main{
-    public static void main(String[] args){
-        JOptionPane.showMessageDialog(null, "Все Ок?!");
+public class Main {
+  //  public static void main(String[] args) {
+  //      String name;
+  //      name = JOptionPane.showInputDialog("Добрый день! Как Вас зовут?");
+  //      JOptionPane.showMessageDialog(null, "Приятно познакомиться,\n" + name + "!");
+
+    public static void main(String[] args) {
+        int number = Integer.parseInt(JOptionPane.showInputDialog("Введите число:"));
+        String text = number % 2 == 0 ? "четное" : "нечетное";
+        JOptionPane.showMessageDialog(null, "Вы ввели " + text + " число!");
     }
 }
+
